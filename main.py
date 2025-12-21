1061f7094e944439fb02c1e1ff906ad34bd951f4
SWStribling <sstri005@gmail.com>
2025-12-21 14:50:31 -0600

    Step 3: data_check

--- main.py
+++ main.py
@@ -74,7 +74,20 @@ def go(config: DictConfig):
             ##################
             # Implement here #
             ##################
-            pass
+            component_path = os.path.abspath(os.path.join("src", "data_check"))
+
+            _ = mlflow.run(
+                component_path,
+                "main",
+                env_manager="conda",
+                parameters={
+                    "csv": 'clean_sample.csv:latest',
+                    "ref": 'clean_sample.csv:reference',
+                    "kl_threshold": config["data_check"]["kl_threshold"],
+                    "min_price": config["etl"]["min_price"],
+                    "max_price": config["etl"]["max_price"]
+                },
+            )
 
         if "data_split" in active_steps:
             ##################
